6b7ebf939b933b3d408082ea4453352454ae926b
jiazeyu1987 <252451895@qq.com>
2020-07-09 17:03:24 +0800

    2020-7-9

---
+++ data_structrue/__init__.py
@@ -0,0 +1,1 @@
+from .sound_tree import *
---
+++ data_structrue/sound_tree/sound_tree_main.py
@@ -0,0 +1,9 @@
+'''
+声音树的特点
+1. 不能逐个进行分析，zai gan shen me VS zai wo yi duan lu
+2. 可能有不同的结果，也就是歧义的句子 bai chi yi yang 白痴一样 VS 白吃一样
+3. 需要和其他数据结构配合
+'''
+class SoundTreeMain:
+    def __init__(self):
+        pass
